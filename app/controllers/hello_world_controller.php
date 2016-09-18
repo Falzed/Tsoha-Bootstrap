e076bbb48da865acdc711d6c80528f3b86efb6db
Falzed <oskari_kulmala@hotmail.com>
2016-09-18 20:30:45 +0300

    näkymiä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -12,4 +12,20 @@
       // Testaa koodiasi täällä
       View::make('helloworld.html');
     }
+    
+    public static function kirjautuminen(){
+      View::make('suunnitelmat/kirjautuminen.html');
+    }
+    public static function rekisteroityminen(){
+      View::make('suunnitelmat/rekisteroityminen.html');
+    }
+    public static function muokkaus(){
+      View::make('suunnitelmat/muokkaus.html');
+    }
+    public static function listaus(){
+      View::make('suunnitelmat/listaus.html');
+    }
+    public static function askare(){
+      View::make('suunnitelmat/askare.html');
+    }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,20 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/askareet', function() {
+    HelloWorldController::listaus();
+  });
+  
+  $routes->get('/askareet/1', function() {
+    HelloWorldController::askare();
+  });
+  $routes->get('/login', function() {
+    HelloWorldController::kirjautuminen();
+  });
+  $routes->get('/register', function() {
+    HelloWorldController::rekisteroityminen();
+  });
+  $routes->get('/askareet/1/modify', function() {
+    HelloWorldController::muokkaus();
+  });
