0e087d032c53433d0324e7e6d93e2044b0434850
Falzed <oskari_kulmala@hotmail.com>
2016-09-18 20:44:30 +0300

    create/drop tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,15 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    password varchar(50) NOT NULL
+);
+
+CREATE TABLE Askare(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES Kayttaja(id),
+    name varchar(50) NOT NULL,
+    description varchar(400),
+    prioriteetti INTEGER,
+    luokat varchar(500),
+    added DATE
+);
