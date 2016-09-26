9291c830fa2c99f14d628181633970fe8df7c71e
Oskari <Oskari Kulmala>
2016-09-26 20:20:44 +0300

    korjattu kuvauksen ja luokkien listaus

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE Askare(
     id SERIAL PRIMARY KEY,
     kayttaja_id INTEGER REFERENCES Kayttaja(id),
     nimi varchar(50) NOT NULL,
-    description varchar(400),
+    description varchar(500),
     prioriteetti INTEGER,
     luokat varchar(500),
     added DATE
